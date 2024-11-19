a40223447c70a5f6dca5267b7aabff6ad8df65f5
ds7 <do.samuels@student.han.nl>
2024-11-19 15:07:20 +0100

    Added environment variables, added .env to .gitignore

--- src/smartMirrorProject/theMirror/views.py
+++ src/smartMirrorProject/theMirror/views.py
@@ -5,6 +5,20 @@ import requests
 import spotipy
 from spotipy import SpotifyOAuth
 
+import os
+
+def fetch_recommended_songs():
+    scope = "user-library-read"
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    results = sp.current_user_saved_tracks()
+    tracks = []
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        tracks.append(f"{idx} {track['artists'][0]['name']} – {track['name']}")
+    return tracks
+
 def fetch_news():
     feed_url = "https://www.nu.nl/rss"
     feed = feedparser.parse(feed_url)
@@ -24,34 +38,6 @@ def time_api(request):
         "dayOfWeek": data["dayOfWeek"],
     }
     return time_data
-
-def fetch_current_song(request):
-    token = request.session.get("spotify_token")
-    if not token:
-        return {"song": "Not logged in", "artist": "N/A"}
-
-    sp = spotipy.Spotify(auth=token)
-    current_track = sp.current_playback()
-    if not current_track or not current_track['is_playing']:
-        return {"song": "No song playing", "artist": "N/A"}
-
-    item = current_track['item']
-    return {
-        "song": item['name'],
-        "artist": ", ".join([artist['name'] for artist in item['artists']])
-    }
-    
-def exchange_code_for_token(request):
-    code = request.GET.get("code")
-    if not code:
-        return HttpResponse("No code provided", status=400)
-
-    try:
-        token_info = sp_oauth.get_access_token(code)
-        request.session["spotify_token"] = token_info["access_token"]
-        return HttpResponse("Token successfully retrieved and stored in session")
-    except Exception as e:
-        return HttpResponse(f"Error retrieving token: {str(e)}", status=500)
 
 def index(request):
     # Example data with widget types or specific templates
@@ -151,19 +137,20 @@ def index(request):
             "type": "weather",
             "data": {"temperature": 21, "condition": "Windy"},
         },
+        "Time": {
+            "id": 21,
+            "type": "time",
+            "data": time_api(request)
+        },          
         "News": {
             "id": 22,
             "type": "news",
             "data": fetch_news()
         },
-        "Time": {
-            "type": "time",
-            "data": time_api(request)
-        },
         "Music": {
             "id": 23,
             "type": "music",
-            "data": fetch_current_song(request)
+            "data": fetch_recommended_songs()
         },
     }
     context = {"widgets": widgets}

6dda74d40b29ff78886f85d40ae3f418dc341094
thor <TWM.Hol@student.han.nl>
2024-11-20 16:30:17 +0100

    made system so clock syncs with api every hour

--- src/smartMirrorProject/theMirror/views.py
+++ src/smartMirrorProject/theMirror/views.py
@@ -2,27 +2,55 @@ import feedparser
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
-    
-    
+import time
+import datetime
+
+
 def fetch_news():
     feed_url = "https://www.nu.nl/rss"
     feed = feedparser.parse(feed_url)
-    news_data = [{"title": entry.title, "link": entry.link} for entry in feed.entries[:2]]
+    news_data = [
+        {"title": entry.title, "link": entry.link} for entry in feed.entries[:2]
+    ]
     return news_data
 
+
+cached_time_data = {}
+last_fetched_time = 0
+
+
 def time_api(request):
-    response = requests.get(
-        "https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam",
-        timeout=10,
-    )
-    data = response.json()
-    time_data = {
-        "time": data["time"],
-        "seconds": data["seconds"],
-        "date": data["date"],
-        "dayOfWeek": data["dayOfWeek"],
-    }
-    return time_data
+    global cached_time_data
+    global last_fetched_time
+    elapsed_seconds = int(time.time() - last_fetched_time)
+    if not cached_time_data or (time.time() - last_fetched_time) > 3600:  # 1 hour
+        response = requests.get(
+            "https://www.timeapi.io/api/time/current/zone?timeZone=Europe%2FAmsterdam",
+            timeout=10,
+        )
+        data = response.json()
+        cached_time_data = {
+            "hour": data["hour"],
+            "minute": data["minute"],
+            "seconds": data["seconds"],
+            "date": data["date"],
+            "dayOfWeek": data["dayOfWeek"],
+        }
+        last_fetched_time = time.time()
+    else:
+        cached_time_data["seconds"] += 1
+        print(elapsed_seconds)
+        if cached_time_data["seconds"] >= 59:
+            cached_time_data["seconds"] = 0
+            cached_time_data["minute"] += 1
+        if cached_time_data["minute"] >= 59:
+            cached_time_data["minute"] = 0
+            cached_time_data["hour"] += 1
+        if cached_time_data["hour"] >= 23:
+            cached_time_data["hour"] = 0
+
+    return cached_time_data
+
 
 def index(request):
     # Example data with widget types or specific templates
@@ -127,15 +155,8 @@ def index(request):
             "type": "weather",
             "data": {"temperature": 21, "condition": "Windy"},
         },
-        "News": {
-            "id": 22,
-            "type": "news",
-            "data": fetch_news()
-        },
-        "Time": {
-            "type": "time",
-            "data": time_api(request)
-        },
+        "News": {"id": 22, "type": "news", "data": fetch_news()},
+        "Time": {"type": "time", "data": time_api(request)},
     }
     context = {"widgets": widgets}
     return render(request, "theMirror/index.html", context)

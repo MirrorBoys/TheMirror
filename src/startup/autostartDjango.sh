91c680bc8b69cc99eb0f2843b5e635fa218265a4
Thorsten3 <93926274+Thorsten3@users.noreply.github.com>
2024-11-25 09:55:24 +0100

    Update autostartDjango.sh

--- src/startup/autostartDjango.sh
+++ src/startup/autostartDjango.sh
@@ -5,6 +5,6 @@ git pull
 
 cd ~/TheMirror/src/smartMirrorProject
 source ~/.virtualEnvs/theMirrorEnv/bin/activate
-python manage.py  runserver 0.0.0.0:8000 &
+python3 manage.py  runserver 0.0.0.0:8000 &
 
 exit 0

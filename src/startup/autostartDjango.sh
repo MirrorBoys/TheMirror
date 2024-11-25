daa0338715b28f03590057489bce3c93f7c58a8b
thor <TWM.Hol@student.han.nl>
2024-11-25 13:46:03 +0100

    added pip install to make sure that packages are up to date

--- src/startup/autostartDjango.sh
+++ src/startup/autostartDjango.sh
@@ -4,6 +4,7 @@ cd ~/TheMirror
 git pull
 
 cd ~/TheMirror/src/smartMirrorProject
+pip install -r ~/TheMirror/src/smartMirrorProject/requirements.txt --no-cache-dir
 source ~/.virtualEnvs/theMirrorEnv/bin/activate
 python3 manage.py  runserver 0.0.0.0:8000 &
 

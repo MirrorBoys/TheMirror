f754983ed1244b7109f3876ca012ddd0df9f6062
Bart <93925154+BvdG-HAN@users.noreply.github.com>
2024-11-27 00:57:48 +0100

    Added homepage

--- src/smartMirrorProject/homePage/views.py
+++ src/smartMirrorProject/homePage/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+# Settings for all widgets
+API_TIMEOUT = 10
+
+# Weather widget settings
+WEATHER_NUMBER_OF_DAYS = 2
+
+def index(request):
+    """
+    Renders the homepage with the specified widgets. Each widget needs these keys: 
+        id (int): The widget ID.
+        type (str): The type of the widget.
+        data (dict): The weather data fetched from the API.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: The rendered homepage with the widgets context.
+    """
+    widgets = {
+    }
+
+    context = {"widgets": widgets}
+    return render(request, 'homePage/index.html', context)

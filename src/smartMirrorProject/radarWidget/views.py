6ce9e73e79139e9f1e94e91cfc1ba98445fb0f44
briandreuning <b.dreuning@student.han.nl>
2024-12-02 15:35:24 +0100

    Basiswidget buienradar toegevoegd

--- src/smartMirrorProject/radarWidget/views.py
+++ src/smartMirrorProject/radarWidget/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def fetch_weather_radar(request):
-    niks = "niks"
-    return niks
-    
+   placeholder = "placeholder"
+   return placeholder

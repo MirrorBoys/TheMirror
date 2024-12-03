8173275f3da3851cefc4510a8cfa80f8a084d58b
Darren Samuels <do.samuels@student.han.nl>
2024-12-03 14:57:19 +0100

    Removed seconds

--- src/smartMirrorProject/timeWidget/views.py
+++ src/smartMirrorProject/timeWidget/views.py
@@ -6,7 +6,7 @@ def current_time(request, timezone):
     decoded_timezone = timezone.replace("-", "/")
     tz = pytz.timezone(decoded_timezone) 
     now = datetime.now(tz)
-    current_time = now.strftime("%d-%m-%Y %H:%M:%S")
+    current_time = now.strftime("%d-%m-%Y %H:%M")
     return JsonResponse({"current_time": current_time, "timezone": str(tz)})
 
 # Function to get timezone from the session (defined in views.py in homePage app) to the time widget

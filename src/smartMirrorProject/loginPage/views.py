199aa8aa25bb60278caae05ae2272a81df000971
Bart <93925154+BvdG-HAN@users.noreply.github.com>
2024-12-04 11:57:42 +0100

    Added loginpage without functionality

--- src/smartMirrorProject/loginPage/views.py
+++ src/smartMirrorProject/loginPage/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("This is a test.")
+    context = {"widgets": None}
+    return render(request, "loginPage/index.html", context)

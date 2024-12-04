b0a34c72235f4f21ecde8dba0911a5e6c32dfad7
Darren Samuels <do.samuels@student.han.nl>
2024-12-04 11:24:05 +0100

    Cleanup

--- src/smartMirrorProject/noteWidget/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("fetch/", views.fetch_notes, name="fetchNotes"),
-]
--- src/smartMirrorProject/noteWidget/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import JsonResponse
-
-# Create your views here.
-def fetch_notes(request):
-    notes = "test"
-    return JsonResponse(list(notes), safe=False)

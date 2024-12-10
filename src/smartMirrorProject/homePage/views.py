5a0d2b6420311ed392c645fc0fc6b95d60fe983e
DarrenSamuels <do.samuels@student.han.nl>
2024-12-10 16:04:37 +0100

    Merged main with this branchh

--- src/smartMirrorProject/homePage/views.py
+++ src/smartMirrorProject/homePage/views.py
@@ -3,55 +3,109 @@ import requests
 import os
 import yaml
 
-
-config_file_path = os.path.join(os.path.dirname(__file__), "..", "config.yml")
-with open(config_file_path, "r") as file:
-    config = yaml.safe_load(file)
-
+CONFIG_FILE_PATH = os.path.join(os.path.dirname(__file__), "..", "config.yml")
+with open(CONFIG_FILE_PATH, "r") as file:
+    CONFIG = yaml.safe_load(file)
 
 # Settings for all widgets
-API_TIMEOUT = config["general_settings"]["API_TIMEOUT"]
+API_TIMEOUT = CONFIG["general_settings"]["API_TIMEOUT"]
 
 # Weather widget settings
-WEATHER_NUMBER_OF_DAYS = config["weather"]["WEATHER_NUMBER_OF_DAYS"]
+WEATHER_NUMBER_OF_DAYS = CONFIG["weather"]["WEATHER_NUMBER_OF_DAYS"]
 
 # News widget settings
-NEWS_NUMBER_OF_ARTICLES = config["news"]["NEWS_NUMBER_OF_ARTICLES"]
+NEWS_NUMBER_OF_ARTICLES = CONFIG["news"]["NEWS_NUMBER_OF_ARTICLES"]
 
 # Time widget settings
-TIMEZONE = "Europe/Amsterdam" # use TZ identifier (e.g. Europe/Amsterdam) or TZ database name (e.g. CET)
-ENCODED_TIMEZONE = TIMEZONE.replace("/", "-")
+TIME_TIMEZONE = CONFIG["time"]["TIMEZONE"]
+TIME_ENCODED_TIMEZONE = TIME_TIMEZONE.replace("/", "-")
 
-# Travel widget settings
-
-TRAVEL_JOURNEY_BEGIN_STATION = config["travel"]["TRAVEL_JOURNEY_BEGIN_STATION"]
-TRAVEL_JOURNEY_END_STATION = config["travel"]["TRAVEL_JOURNEY_END_STATION"]
-TRAVEL_JOURNEY_NUMBER_OF_TRIPS = config["travel"]["TRAVEL_JOURNEY_NUMBER_OF_TRIPS"]
-TRAVEL_DEPARTURES_STATION = config["travel"]["TRAVEL_DEPARTURES_STATION"]
-TRAVEL_DEPARTURES_FILTER = config["travel"]["TRAVEL_DEPARTURES_FILTER"]
-RADAR_CITY = "Arnhem"
-
-# Notes widget settings
-NOTES = [
-    {
-        "title": "test1",
-        "type": "ul",
-        "subType": "li",
-        "content": [
-            "test test test test test test test test test test test test test test test test test test test test test",
-            "It can have multiple lines, paragraphs, bulletpoints etc."
-        ]
-    },
-    {
-        "title": "test23456789",
-        "type": "p",
-        "subType": "",
-        "content": [
-            "hoi",
-            "It can have multiple lines, paragraphs, bulletpoints etc."
-        ]
-    }
+# Travel journeys widget settings
+TRAVEL_JOURNEY_BEGIN_STATION = CONFIG["travel_journeys"]["TRAVEL_JOURNEY_BEGIN_STATION"]
+TRAVEL_JOURNEY_END_STATION = CONFIG["travel_journeys"]["TRAVEL_JOURNEY_END_STATION"]
+TRAVEL_JOURNEY_NUMBER_OF_TRIPS = CONFIG["travel_journeys"][
+    "TRAVEL_JOURNEY_NUMBER_OF_TRIPS"
 ]
+
+# Travel departures widget settings
+TRAVEL_DEPARTURES_STATION = CONFIG["travel_departures"]["TRAVEL_DEPARTURES_STATION"]
+TRAVEL_DEPARTURES_FILTER = CONFIG["travel_departures"]["TRAVEL_DEPARTURES_FILTER"]
+
+# Radar widget settings
+RADAR_CITY = CONFIG["radar"]["RADAR_CITY"]
+
+INTERNAL_API_LINKS = {
+    "agenda": "http://localhost:8000/api/agenda/fetch/",
+    "news": f"http://localhost:8000/api/news/fetch/{NEWS_NUMBER_OF_ARTICLES}",
+    "note": "http://localhost:8000/api/note/fetch/",
+    "travel_journeys": f"http://localhost:8000/api/travel/fetch/journeys/{TRAVEL_JOURNEY_BEGIN_STATION}/{TRAVEL_JOURNEY_END_STATION}/{TRAVEL_JOURNEY_NUMBER_OF_TRIPS}",
+    "travel_departures": f"http://localhost:8000/api/travel/fetch/departures/{TRAVEL_DEPARTURES_STATION}/{TRAVEL_DEPARTURES_FILTER}",
+    "weather": f"http://localhost:8000/api/weather/fetch/{WEATHER_NUMBER_OF_DAYS}",
+    "radar": f"http://localhost:8000/api/radar/fetch/coordinates/{RADAR_CITY}",
+    "time": f"http://localhost:8000/api/time/fetch/{TIME_ENCODED_TIMEZONE}",
+}
+
+
+def createWidgetsObject(config, api_links, api_timeout):
+    """
+    Creates a dictionary of widget objects based on the provided configuration.
+
+    Args:
+        config (dict): A dictionary containing widget configurations.
+        api_links (dict): A dictionary containing API links for each widget.
+        api_timeout (int): The timeout value (in seconds) for API requests.
+
+    Returns:
+        dict: A dictionary where each key is a widget that contains a dictionary with
+              widget settings.
+    """
+    available_widgets = list(config.keys())
+    widgetObject = {}
+
+    # Skip first index because this contains the general_settings
+    for index, widget in enumerate(available_widgets[1:]):
+        if not config[widget]["VISIBLE"]:
+            continue
+
+        app_name = generate_app_name(widget)
+
+        # Custom approach to music widget is needed because it does not use an internal API
+        if widget == "music":
+            widgetObject[widget] = {
+                "id": index,
+                "appName": app_name,
+                "templateName": widget,
+            }
+        else:
+            widgetObject[widget] = {
+                "id": index,
+                "appName": app_name,
+                "templateName": widget,
+                "data": "",
+                "apiCall": lambda link=api_links[widget]: requests.get(
+                    link, timeout=api_timeout
+                ).json(),
+            }
+
+    return widgetObject
+
+
+def generate_app_name(widget_name: str):
+    """
+    Generates an application name based on the provided widget name.
+
+    Args:
+        widget_name (str): The name of the widget.
+
+    Returns:
+        str: The generated application name.
+    """
+    app_name = widget_name + "Widget"
+
+    if "_" in widget_name:
+        app_name = widget_name.split("_")[0] + "Widget"
+
+    return app_name
 
 
 def index(request):
@@ -68,75 +122,16 @@ def index(request):
     Returns:
         HttpResponse: The rendered homepage with the widgets context.
     """
-    widgets = {
-        "weather": {
-            "id": 1,
-            "appName": "weatherWidget",
-            "templateName": "weather",
-            "data": requests.get(
-                f"http://localhost:8000/api/weather/fetch/{WEATHER_NUMBER_OF_DAYS}",
-                timeout=API_TIMEOUT,
-            ).json(),
-        },
-        "news": {
-            "id": 2,
-            "appName": "newsWidget",
-            "templateName": "news",
-            "data": requests.get(
-                f"http://localhost:8000/api/news/fetch/{NEWS_NUMBER_OF_ARTICLES}",
-                timeout=API_TIMEOUT,
-            ).json(),
-        },
-        "travel-journeys": {
-            "id": 3,
-            "appName": "travelWidget",
-            "templateName": "travel-journeys",
-            "data": requests.get(
-                f"http://localhost:8000/api/travel/fetch/journeys/{TRAVEL_JOURNEY_BEGIN_STATION}/{TRAVEL_JOURNEY_END_STATION}/{TRAVEL_JOURNEY_NUMBER_OF_TRIPS}",
-                timeout=API_TIMEOUT,
-            ).json(),
-        },
-        "travel-departures": {
-            "id": 4,
-            "appName": "travelWidget",
-            "templateName": "travel-departures",
-            "data": requests.get(
-                f"http://localhost:8000/api/travel/fetch/departures/{TRAVEL_DEPARTURES_STATION}/{TRAVEL_DEPARTURES_FILTER}",
-                timeout=API_TIMEOUT,
-            ).json(),
-        },
-        "music": {
-            "id": 5,
-            "appName": "musicWidget",
-            "templateName": "music",
-            "data": "",
-        },
-        "time": {
-            "id": 5,
-            "appName": "timeWidget",
-            "templateName": "time",	
-            "data": requests.get(f"http://localhost:8000/api/time/fetch/{ENCODED_TIMEZONE}", timeout=API_TIMEOUT).json(),
-        },
-        "agenda": {
-            "id": 6,
-            "appName": "agendaWidget",
-            "templateName": "agenda",
 
-            "data": requests.get("http://localhost:8000/api/agenda/fetch/", timeout=API_TIMEOUT).json()["events"],
-        },
-        "radar": {
-            "id": 7,
-            "appName": "radarWidget",
-            "templateName": "radar",
-            "data": requests.get(f"http://localhost:8000/api/radar/fetch/coordinates/{RADAR_CITY}", timeout=API_TIMEOUT).json(),
-        },
-        "notes": {
-            "id": 8,
-            "appName": "noteWidget",
-            "templateName": "notes",
-            "data": requests.get(f"http://localhost:8000/api/note/fetch/", timeout=API_TIMEOUT).json(),
-        },
-    }
+    widgets = createWidgetsObject(CONFIG, INTERNAL_API_LINKS, API_TIMEOUT)
+
+    # Using the internal API's, generate data for each widget.
+    # Skip generation of data for music widget since it does not use internal generated data
+    for widget in widgets.values():
+        if widget["appName"] == "musicWidget":
+            continue
+
+        widget["data"] = widget["apiCall"]()
 
     context = {"widgets": widgets}
     return render(request, "homePage/index.html", context)
--- src/smartMirrorProject/noteWidget/views.py
+++ src/smartMirrorProject/noteWidget/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from homePage.views import NOTES
 
 # Create your views here.
 
 def fetch_notes(request):
-    notes = NOTES
+    # For the now the data here is hardcoded. Later this will be fetched from the database.
+    notes = [
+        {
+            "title": "test1234",
+            "type": "ul",
+            "subType": "li",
+            "content": [
+                "test test test test test test test test test test test test test test test test test test test test test",
+                "It can have multiple lines, paragraphs, bulletpoints etc."
+            ]
+        },
+        {
+            "title": "test23456789",
+            "type": "p",
+            "subType": "",
+            "content": [
+                "hoi",
+                "It can have multiple lines, paragraphs, bulletpoints etc."
+            ]
+        }
+    ]
     return JsonResponse(list(notes), safe=False)

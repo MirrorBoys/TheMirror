70b099d8aabc51894a75559bd84b6080eee47544
Bart <93925154+BvdG-HAN@users.noreply.github.com>
2024-12-10 13:00:47 +0100

    Added logout button (CSS wil be done on another branch)

--- src/smartMirrorProject/loginPage/views.py
+++ src/smartMirrorProject/loginPage/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 
 def index(request):
     context = {"widgets": None}
     return render(request, "loginPage/index.html", context)
+
+
+def userLogout(request):
+    logout(request)
+    return redirect("loginPageIndex")
--- src/smartMirrorProject/smartMirrorProject/urls.py
+++ src/smartMirrorProject/smartMirrorProject/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path(
         "login/", LoginView.as_view(template_name="loginPage/index.html"), name="login"
     ),
+    path("logout/", include("loginPage.urls")),
     path("admin/", admin.site.urls),
     path("musicWidget/", include("musicWidget.urls")),
     path("api/weather/", include("weatherWidget.urls")),

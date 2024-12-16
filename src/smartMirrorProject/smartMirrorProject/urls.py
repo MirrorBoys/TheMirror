63eb10f8fda7161fb0004fc49d60409e8bb8377e
Bart <b.vandeGriendt@student.han.nl>
2024-12-16 09:51:02 +0100

    Bugfix for non-functioning loginpage

--- src/smartMirrorProject/smartMirrorProject/urls.py
+++ src/smartMirrorProject/smartMirrorProject/urls.py
@@ -23,7 +23,9 @@ urlpatterns = [
     path("", include("homePage.urls")),
     path("logout/", include("loginPage.urls")),
     path("api/note/", include("noteWidget.urls")),
-    path("login/", include("loginPage.urls")),
+    path(
+        "login/", LoginView.as_view(template_name="loginPage/index.html"), name="login"
+    ),
     path("admin/", admin.site.urls),
     path("api/time/", include("timeWidget.urls")),
     path("musicWidget/", include("musicWidget.urls")),
@@ -32,5 +34,4 @@ urlpatterns = [
     path("api/travel/", include("travelWidget.urls")),
     path("api/agenda/", include("agendaWidget.urls")),
     path("api/radar/", include("radarWidget.urls")),
-
 ]

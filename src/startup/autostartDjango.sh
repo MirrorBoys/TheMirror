2eff0880cd1d4c7d8c091d6b692d0d4b701b82e4
briandreuning <b.dreuning@student.han.nl>
2024-12-17 10:45:54 +0100

    autostart script aangepast met comments & runnen van camerascript

--- src/startup/autostartDjango.sh
+++ src/startup/autostartDjango.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Navigate to the main project directory and update the repository
 cd ~/TheMirror
 git pull
 
+# Navigate to the smart mirror project directory and activate the virtual environment
 cd ~/TheMirror/src/smartMirrorProject
 source ~/.virtualEnvs/theMirrorEnv/bin/activate
+
+# Install the required Python packages
 pip3 install -r ~/TheMirror/src/smartMirrorProject/requirements.txt --no-cache-dir
-python3 manage.py  runserver 0.0.0.0:8000 &
+
+# Start the Django server
+python3 manage.py runserver 0.0.0.0:8000 &
+
+# Run the gesture utility script
+python3 ~/TheMirror/src/smartMirrorProject/utilities/gestureUtility/testscript.py &
 
 exit 0

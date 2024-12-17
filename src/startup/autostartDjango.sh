ea4dc332b5b2e60bc9814534671dfcd34ff02127
briandreuning <b.dreuning@student.han.nl>
2024-12-17 11:56:54 +0100

    lxterminal toegevoegd, die een terminal opent en het script runt

--- src/startup/autostartDjango.sh
+++ src/startup/autostartDjango.sh
@@ -16,7 +16,7 @@ pip3 install -r ~/TheMirror/src/smartMirrorProject/requirements.txt --no-cache-d
 python3 manage.py runserver 0.0.0.0:8000 &
 
 # Run the gesture utility script
-python3 ~/TheMirror/src/smartMirrorProject/utilities/gestureUtility/testscript.py > ~/gestureUtility.log 2>&1
+lxterminal -e "python3 ~/TheMirror/src/smartMirrorProject/utilities/gestureUtility/testscript.py"
 
 
 exit 0

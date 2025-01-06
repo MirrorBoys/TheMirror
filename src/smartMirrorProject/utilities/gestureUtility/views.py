40306c5320ca8e6b06bf6c30da02018b908348c4
briandreuning <b.dreuning@student.han.nl>
2025-01-06 13:58:39 +0100

    Views.py aangepast op gestures

--- src/smartMirrorProject/utilities/gestureUtility/views.py
+++ src/smartMirrorProject/utilities/gestureUtility/views.py
@@ -7,20 +7,6 @@ from . import testscript
 # Imagine this would be a hand gesture recognition function that would set the global variable gesture to the gesture that was recognized.
 gesture = ''
 
-def setGestureData():
-    global gesture
-    action = None
-    if gesture == 'fist':
-        action = 'PAUSE'
-    elif gesture == 'one':
-        action = 'PLAY'
-    elif gesture == 'two':
-        action = 'SKIP'
-    elif gesture == 'l':
-        action = 'LOGIN'
-    
-    return action
-
 def sendGestureData(request):
     gestureData = setGestureData()
     
@@ -31,3 +17,27 @@ def sendGestureData(request):
     gesture = None
     
     return JsonResponse(response_data)
+
+def setGestureData():
+    global gesture
+    action = None
+    if gesture == is_index_up and are_middle_ring_pinky_down:
+        print("Gesture Detected: Index Finger Up!")
+        action = 'LOGIN'
+    elif gesture == is_middle_up and are_index_ring_pinky_down:
+        print("Gesture Detected: You are very rude!!!") 
+        action = 'SHUTDOWN'
+    elif gesture == are_all_fingers_up:
+        print("Gesture Detected: All Fingers Up!")        
+        action = 'PAUSE'
+    elif gesture == is_my_thumb_up:
+        print("Gesture Detected: Like And Subscribe")   
+        action = 'PLAY'
+    elif gesture == is_pinky_up and is_my_pinky_up:
+        print("Gesture Detected: Pinky Up!")    
+        action = 'REFRESH'
+    elif gesture == is_index_pointing_left:
+        print("Gesture Detected: Point Right!")
+        action = 'SKIP'
+    
+    return action

30a623c542d4cc39e85d514b549825d4f89badfa
DarrenSamuels <do.samuels@student.han.nl>
2025-01-07 11:22:21 +0100

    Another fix autostartdjango

--- src/startup/autostartDjango.sh
+++ src/startup/autostartDjango.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
 export DISPLAY=:0
+
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+# Redirect all output to a log file in the same directory as the script
+LOG_FILE="$SCRIPT_DIR/autostart.log"
+exec > "$LOG_FILE" 2>&1
+
 # Navigate to the main project directory and update the repository
 cd ~/TheMirror
-git checkout 190-realiseren-achterliggende-functies-voor-gebaren 
+git checkout 190-realiseren-achterliggende-functies-voor-gebaren
 git pull
 
 # Navigate to the smart mirror project directory and activate the virtual environment

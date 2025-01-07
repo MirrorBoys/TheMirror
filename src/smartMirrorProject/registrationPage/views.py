a63fde9e7015e1e47ad4100f6f69e6df72bcec16
Bart van de Griendt <b.vandeGriendt@student.han.nl>
2025-01-07 12:42:00 +0100

    Added basic registration without adding NFC tag

--- src/smartMirrorProject/registrationPage/views.py
+++ src/smartMirrorProject/registrationPage/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class CustomUserCreationForm(UserCreationForm):
+    """
+    CustomUserCreationForm is a form for creating new users, extending the built-in UserCreationForm. Needed because we use an custom implementation
+    of Django's build-in accounts.
+    """
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
 
 
 def register(request):
-
-    return render(request, "registrationPage/register.html", {"temp": "temp"})
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("homePageIndex")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registrationPage/register.html", {"form": form})
